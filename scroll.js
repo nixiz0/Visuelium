a870610f60938595ba22afa06c182e5dc5bd8b85
Julian <julian.57@outlook.fr>
2022-11-05 01:46:50 +0100

    Presentation intro design

---
+++ scroll.js
@@ -0,0 +1,10 @@
+/*window.addEventListener('scroll', () => {
+    document.getElementById("who");
+
+    if(window.scrollY > 501) {
+        who.style.position = "fixed";
+    }
+    else if(window.scrollY < 100) {
+        who.style.position = "relative";
+    }
+}); */
