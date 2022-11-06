cc662cbd4f8c54c8cbfacfc89664c041edae78b5
Julian <julian.57@outlook.fr>
2022-11-06 01:52:57 +0100

    Established the Netlify Form

--- mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (isset($_POST['message'])) {
-        $retour = mail('maquette.initium@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: visuelium@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
-        if($retour)
-            echo '<p>Votre message a bien été envoyé.</p>';
-    }
-    
