09538d6fae40c384732ffc2460994da21225ce9f
Julian <julian.57@outlook.fr>
2022-11-06 01:44:04 +0100

    test

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
     if (isset($_POST['message'])) {
-        $retour = mail('maquette.initium@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
+        $retour = mail('maquette.initium@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: visuelium@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
     }
