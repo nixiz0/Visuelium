bbcb1f72848729659be1e5130565c7be98e206e5
Julian <julian.57@outlook.fr>
2022-11-06 01:36:27 +0100

    try to connect php for my contact form

--- mail.php
+++ mail.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-    <?php
+<?php
     if (isset($_POST['message'])) {
-        $retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
+        $retour = mail('maquette.initium@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
     }
-    ?>
-
-</body>
-</html>
+    
