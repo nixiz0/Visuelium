5d678ed8bf9cf5a5ae4e020a625beb9e2190a61d
Julian <julian.57@outlook.fr>
2022-11-06 14:25:30 +0100

    rename the JS file

--- effects.js
+++ effects.js
@@ -1,13 +1,3 @@
-/*window.addEventListener('scroll', () => {
-    document.getElementById("who");
-
-    if(window.scrollY > 501) {
-        who.style.position = "fixed";
-    }
-    else if(window.scrollY < 100) {
-        who.style.position = "relative";
-    }
-}); */
 
 /* Slide Effects */
 let img__slider = document.getElementsByClassName('img__slider');
